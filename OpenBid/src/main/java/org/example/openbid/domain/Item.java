cd782bcccad6ca2edc999e0b15669f6a32671f0b
Istudor Mihai <istudor9@gmail.com>
2025-04-25 20:25:33 +0300

    Added User and Item domains

---
+++ OpenBid/src/main/java/org/example/openbid/domain/Item.java
@@ -0,0 +1,23 @@
+package org.example.openbid.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    private String name;
+    private Double price;
+    private String category;
+    private Date date;
+    private User bidder;
+    private User owner;
+    private String image;
+    private String description;
+}
+
--- OpenBid/src/main/java/org/example/openbid/domain/test.java
+++
@@ -1,4 +0,0 @@
-package org.example.openbid.domain;
-
-public class test {
-}
