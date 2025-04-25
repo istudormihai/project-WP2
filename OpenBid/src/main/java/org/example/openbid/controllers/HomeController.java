ac8ac84917e0977147ec6adfb2b5e4470373e41d
Istudor Mihai <istudor9@gmail.com>
2025-04-25 21:24:37 +0300

    added the contact page

--- OpenBid/src/main/java/org/example/openbid/controllers/HomeController.java
+++ OpenBid/src/main/java/org/example/openbid/controllers/HomeController.java
@@ -22,4 +22,9 @@ public class HomeController {
     public String register() {
         return "register";
     }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
 }
