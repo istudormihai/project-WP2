e68b4e8656af8f169d60763e7fc1a585a9cb6865
septimium <septimiutg@gmail.com>
2025-04-25 19:24:02 +0300

    Created Empty Spring Project

---
+++ OpenBid/src/main/java/org/example/openbid/OpenBidApplication.java
@@ -0,0 +1,13 @@
+package org.example.openbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenBidApplication.class, args);
+    }
+
+}
---
+++ OpenBid/src/test/java/org/example/openbid/OpenBidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.openbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
