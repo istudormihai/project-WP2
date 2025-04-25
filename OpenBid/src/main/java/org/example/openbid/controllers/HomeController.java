86ecf272e7eaf4d21a16ae9d0b188c91b18a6092
septimium <septimiutg@gmail.com>
2025-04-25 20:47:25 +0300

    Added login and register pages

--- OpenBid/src/main/java/org/example/openbid/controllers/HomeController.java
+++ OpenBid/src/main/java/org/example/openbid/controllers/HomeController.java
@@ -8,10 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
-    public String home(){
-        //returns the view
-        return "index";
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
